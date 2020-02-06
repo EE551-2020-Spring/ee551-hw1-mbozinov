9dba36705d9b41613d50833c0943339f76fb28ed
Rensheng-Wang <58668982+Rensheng-Wang@users.noreply.github.com>
2020-02-06 17:24:18 -0500

    Initial commit

---
+++ EE551_HW1.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+
+"""
+Python Core object Types
+"""
+
+import math
+
+def numbers_and_strings():
+    """
+    This is to review numbers and strings and basic operations.
+    """
+    # Assign a string "EE551" to a variable x
+    x = "EE551"
+
+    # Assign a string "Stevens" to a variable y
+
+    # Repeat variable y 5 times
+
+    # What is the length of z?
+
+    # Concatenate variable y with string " is good"
+
+    # Replace "good" with "awesome" in variable m and assign it to a new variable n
+
+    return x, y, z, length, m, n
+
+
+def lists():
+    """
+    This is to review basic operations with lists.
+    """
+    n = "Stevens is awesome"
+
+    # Split variable n on a delimiter space into a list of substrings
+
+    # Get all the items past the first of the third substring
+
+    # Create a 3 x 3 matrix as nested list such that
+    #   first row is [1, 4, 5]
+    #   second row is [6, 10, 11]
+    #   third row is [12, 17, 38]
+
+    # Collect the items in the last column of matrix A using list comprehension
+
+    # Collect only the even items of the diagonal of matrix A using list comprehension
+
+    # We can convert a single character to its underlying integer code (e.g., its ASCII byte value)
+    # by passing it to the built-in ord function. Generate a list of these integers to represent
+    # each character of the string "Stevens" using list comprehension.
+
+    return p, r, c, d, o
+
+
+def dictionaries():
+    """
+    This is to review basic operations with dictionaries.
+    """
+    # Create a dictionary that maps:
+    #   fruit => "apple"
+    #   quantity => 4
+    #   color => "green"
+
+    # Get the item in dictionary f that the key "fruit" maps to
+
+    # Increase the quantity of f by 1
+    # IMPLEMENT IT HERE
+
+    # Create a nested dictionary where:
+    #   name => {first_name => "Grace", last_name => "Hopper"} (a dictionary)
+    #   jobs => ["scientist", "engineer"] (a list)
+    #   age => 85
+
+    # Add "programmer" to the list of jobs Grace has
+    # IMPLEMENT IT HERE
+
+    # Get the third job Grace has that you recently added
+
+    # Use the sort() function to get sorted keys of amazing_grace in alphabetically ascending order
+
+    return a, f, p, k
+
+numbers_and_strings()
+lists()
+dictionaries()
+
+
+
+
+
